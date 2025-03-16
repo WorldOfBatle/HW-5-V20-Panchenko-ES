f2a629fed389da1fbd6cc5e6212201624e92ed14
Evgeny Panchenko <worldbatle@mail.ru>
2025-03-16 20:11:09 +0300

    Создана заготовка проекта с функциями

---
+++ HW-5/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Заготовки задач
+void task1()
+{
+}
+
+void task2()
+{
+}
+
+void task3()
+{
+}
+
+void task4()
+{
+}
+
+int main()
+{
+    // Пока вызовем только task1() для теста
+    task1();
+    return 0;
+}
